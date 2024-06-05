44cf5513c43f25a258cdb1bdc1dbb02ede4f118e
Yashwanth <yasw2875@colorado.edu>
2024-06-05 01:01:47 -0600

    feat: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Homepage from "@/components/Homepage";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import {FaHome} from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="mx-2-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome/>},
+        ]}/>
         <Homepage/>
       </div>
     </main>
